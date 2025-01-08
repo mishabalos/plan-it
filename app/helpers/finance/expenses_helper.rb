450743f2441015cb3f62c80380860c0f217d67d0
Misha Abalos <abalos.mishaela@gmail.com>
2025-01-08 19:26:03 +0800

    frontend updates

--- app/helpers/finance/expenses_helper.rb
+++ app/helpers/finance/expenses_helper.rb
@@ -1,2 +1,13 @@
 module Finance::ExpensesHelper
+  def category_icon(category)
+    # Define your icon logic here
+    # For example:
+    case category
+    when "food"
+      "food-icon"
+    when "transport"
+      "transport-icon"
+      # etc...
+    end
+  end
 end
